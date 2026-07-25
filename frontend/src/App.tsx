b8b3ff434607ba4513f5d664d47fe006b0d3f480
achaekevin <achaekevin@gmail.com>
2026-07-25 21:37:49 +0300

    feat(localization): wrap App with LocalizationProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import { LocalizationProvider } from '@/contexts/localization-context'
 import { queryClient } from '@/lib/query-client'
 import { router } from '@/routes'
 
@@ -10,9 +11,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-right" richColors />
-        <ReactQueryDevtools initialIsOpen={false} />
+        <LocalizationProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" richColors />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </LocalizationProvider>
       </ThemeProvider>
     </QueryClientProvider>
   )
